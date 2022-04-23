b56ebbbda0f0fd2bc75c2f35d95b2253595751be
3m5suchandt <jan.suchandt@3m5.de>
2022-04-23 20:22:03 +0200

    Add example css to backend

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-(static function() {})();
+(static function() {
+    // Add stylesheet directories to backend (just an example for now)
+    $GLOBALS['TBE_STYLES']['skins']['enhanced_backend']['stylesheetDirectories']['basic'] = 'EXT:enhanced_backend/Resources/Public/Styles/';
+})();
