b613b85a303b3409f5c938cb7988fba8456c4580
schneider.n <Nicole.Schneider@3m5.de>
2023-01-27 15:05:01 +0100

    add initial content tree js

--- Resources/Public/JavaScript/Backend.js
+++ Resources/Public/JavaScript/Backend.js
@@ -1,2 +1,23 @@
-window.addEventListener('load', (event) => {document.querySelector('body').classList.add('test')});
-console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa')
+window.addEventListener('load', (event) => {
+  const $pageNavigation = document.querySelector('.t3js-scaffold-content-navigation');
+  if(!!$pageNavigation) {
+
+    const contentTree = document.createElement("div");
+    contentTree.classList.add('content-tree');
+    contentTree.innerHTML = 'Content Tree';
+
+    // Get elements with class "example"
+    let gridElements = document.getElementsByClassName(".t3-grid-cell");
+    // Create an empty string to store the HTML list
+    let htmlList = "<ul>";
+    // Loop through the elements
+    for (let i = 0; i < gridElements.length; i++) {
+      console.log(gridElements[i].find('.t3-page-column-header'));
+      htmlList += "<li>" + gridElements[i].find('.t3-page-column-header').textContent + "</li>";
+    }
+    htmlList += "</ul>";
+
+    contentTree.append(htmlList);
+    $pageNavigation.append(contentTree);
+  }
+});
