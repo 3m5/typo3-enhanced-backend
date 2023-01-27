e6010c3bd5fa0a238aef7f6029847cdc4fd6f2e2
kersten <michael.kersten@3m5.de>
2023-01-27 15:41:12 +0100

    added be groups settings for allowed themes

---
+++ Configuration/TCA/Overrides/be_groups.php
@@ -0,0 +1,22 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+$tmpColumns = [
+    'tx_enhancedbackend_themes' => [
+        'label' => 'LLL:EXT:enhanced_backend/Resources/Private/Language/locallang_be.xlf:be_groups.tx_enhancedbackend_themes',
+        'config' => [
+            'type' => 'select',
+            'renderType' => 'selectCheckBox',
+            'items' => [],
+            'itemsProcFunc' => \DMF\EnhancedBackend\Utility\ThemeUtility::class . '->itemsForBeGroup'
+        ]
+    ],
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_groups', $tmpColumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'be_groups',
+    'tx_enhancedbackend_themes',
+    '',
+    'after:explicit_allowdeny'
+);
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Table structure for table 'be_groups'
+#
+CREATE TABLE be_groups (
+	tx_enhancedbackend_themes text,
+);
