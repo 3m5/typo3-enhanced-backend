60ebcfa5df363167d739aa69724e8582c3a0690f
suchandt <jan.suchandt@3m5.de>
2023-01-27 16:56:09 +0100

    Add dark mode css classes to page render
    THX Steffen <3

--- Classes/Hooks/BackendStyles.php
+++ Classes/Hooks/BackendStyles.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace DMF\EnhancedBackend\Hooks;
@@ -16,7 +17,6 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  *  (c) 2022 3m5. Media GmbH <jan.suchandt@3m5.de>
  *
  **/
-
 class BackendStyles
 {
 
@@ -26,28 +26,42 @@ class BackendStyles
         $renderer->addJsFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/JavaScript/Backend.js'), 'text/javascript', false, false, '', true, '|', false, '');
 
         $themeService = GeneralUtility::makeInstance(ThemeService::class);
-        switch ($themeService->getActiveThemeName())
-        {
-            case ThemeService::THEME_NAME_MODERN:
-                $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Modern.css'));
-                $renderer->setHtmlTag($this->addClasstoHtml($renderer->getHtmlTag(),'enbe enbe-theme enbe-theme--modern'));
-
-                break;
-            case ThemeService::THEME_NAME_VANILLA:
-                $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Vanilla.css'));
-                $renderer->setHtmlTag($this->addClasstoHtml($renderer->getHtmlTag(),'enbe enbe-theme enbe-theme--vanilla'));
-                break;
-            case ThemeService::THEME_NAME_CUSTOM:
-                $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Custom.css'));
-                $renderer->setHtmlTag($this->addClasstoHtml($renderer->getHtmlTag(),'enbe enbe-theme enbe-theme--custom'));
-                break;
+        $bodyCssClasses = ['enbe'];
+        if ($themeService->isAnyThemeSelected()) {
+            $bodyCssClasses[] = 'enbe-theme';
+            switch ($themeService->getActiveThemeName()) {
+                case ThemeService::THEME_NAME_MODERN:
+                    $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Modern.css'));
+                    $bodyCssClasses[] = 'enbe-theme--modern';
+                    break;
+                case ThemeService::THEME_NAME_VANILLA:
+                    $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Vanilla.css'));
+                    $bodyCssClasses[] = 'enbe-theme--vanilla';
+                    break;
+                case ThemeService::THEME_NAME_CUSTOM:
+                    $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Custom.css'));
+                    $bodyCssClasses[] = 'enbe-theme--custom';
+                    break;
+            }
         }
+
         $renderer->addCssFile(GeneralUtility::getFileAbsFileName('EXT:enhanced_backend/Resources/Public/Styles/Dark.css'));
+        if ($themeService->isDarkModeEnabled()) {
+            $bodyCssClasses[] = 'color-mode--dark';
+        }
+        if ($themeService->isLightModeEnabled()) {
+            $bodyCssClasses[] = 'color-mode--light';
+        }
+        if ($themeService->isLSystemModeEnabled()) {
+            $bodyCssClasses[] = 'color-mode--system';
+        }
+
+        $renderer->setHtmlTag($this->addClasstoHtml($renderer->getHtmlTag(), implode(' ', $bodyCssClasses)));
     }
 
     private function addClasstoHtml($htmltag, $class)
     {
-        return str_replace($htmltag,'<html ', '<html class="'.$class.'" ');
+        return str_replace($htmltag, '<html ', '<html class="' . $class . '" ');
     }
 
 }
