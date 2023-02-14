b008f37bdbe79b2c1df75c2d313ee41189ceb6a9
suchandt <jan.suchandt@3m5.de>
2023-02-14 11:02:21 +0100

    T3ENCBE-81 Add draft icon override

---
+++ Configuration/Icons.php
@@ -0,0 +1,46 @@
+<?php
+return [
+    ### TOOLBAR
+    # Sidebar toggle
+    'actions-menu' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Extension.svg',
+    ],
+    # bookmark
+    'apps-toolbar-menu-shortcut' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Extension.svg',
+    ],
+    # cache
+    'apps-toolbar-menu-cache' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Extension.svg',
+    ],
+    # help
+    'apps-toolbar-menu-help' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Extension.svg',
+    ],
+    # system info default
+    'system-information-toolbar-item' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Extension.svg',
+    ],
+    # system info open if any log is available
+    'actions-system-list-open' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Extension.svg',
+    ],
+];
