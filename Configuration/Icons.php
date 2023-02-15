6745a30f2ccf961841cb62be7b0f1723c2ea9859
schneider.n <nicole.schneider@3m5.de>
2023-02-15 17:43:57 +0100

    T3ENCBE-88 add more phosphor icons add styling for icon colors

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    ### TOOLBAR
+    ### SIDEBAR
     # Sidebar toggle
     'actions-menu' => [
         // icon provider class
@@ -8,19 +8,20 @@ return [
         // the source SVG for the SvgIconProvider
         'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Caret-double-left.svg',
     ],
+    ### TOPBAR
     # bookmark
     'apps-toolbar-menu-shortcut' => [
         // icon provider class
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         // the source SVG for the SvgIconProvider
-        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Bookmark.svg',
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Bookmark-simple.svg',
     ],
     # bookmark
     'actions-system-shortcut-new' => [
         // icon provider class
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         // the source SVG for the SvgIconProvider
-        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Bookmark.svg',
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Bookmark-simple.svg',
     ],
     # cache
     'apps-toolbar-menu-cache' => [
@@ -38,13 +39,6 @@ return [
     ],
     # cache frontend in toolbar
     'actions-system-cache-clear-impact-high' => [
-        // icon provider class
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        // the source SVG for the SvgIconProvider
-        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Eraser.svg',
-    ],
-    # cache on page module
-    'actions-system-cache-clear' => [
         // icon provider class
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         // the source SVG for the SvgIconProvider
@@ -71,4 +65,70 @@ return [
         // the source SVG for the SvgIconProvider
         'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Info.svg',
     ],
+    ### TOOLBAR
+    # cache on page module
+    'actions-system-cache-clear' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Eraser.svg',
+    ],
+    # view page in new tab
+    'actions-view-page' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/ArrowSquareOut.svg',
+    ],
+    # edit page properties
+    'actions-page-open' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/NotePencil.svg',
+    ],
+    # copy page url
+    'actions-link' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Link.svg',
+    ],
+    ### PAGE TREE
+    # create new page
+    'apps-pagetree-page-default' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/FilePlus.svg',
+    ],
+    # create new folder
+    'apps-pagetree-folder-default' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/FolderPlus.svg',
+    ],
+    # add bin
+    'apps-pagetree-page-recycler' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Trash.svg',
+    ],
+    # add menu separator
+    'apps-pagetree-spacer' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/Divide.svg',
+    ],
+    # add link to external url
+    'apps-pagetree-page-shortcut-external' => [
+        // icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // the source SVG for the SvgIconProvider
+        'source' => 'EXT:enhanced-backend/Resources/Public/Icons/LinkSimple.svg',
+    ],
+
 ];
