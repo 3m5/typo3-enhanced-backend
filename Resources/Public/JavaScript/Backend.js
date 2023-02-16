ba630388aa3faf4b6c034f3f3fd8733ec7a7057c
schneider.n <nicole.schneider@3m5.de>
2023-02-16 08:28:52 +0100

    T3ENCBE-77 * fix js error and find grid elements

--- Resources/Public/JavaScript/Backend.js
+++ Resources/Public/JavaScript/Backend.js
@@ -1,36 +1,34 @@
-function checkIframeLoaded() {
+function buildContentTree() {
   const iframe = document.querySelector('#typo3-contentIframe');
 
   if (iframe == null) {
-    window.setTimeout(checkIframeLoaded, 1000);
+    window.setTimeout(buildContentTree, 1000);
   } else {
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
     const $pageNavigation = document.querySelector('.t3js-scaffold-content-navigation');
 
     if (iframeDoc.readyState == 'complete' && !!$pageNavigation) {
-      buildContentTree();
+      createContentTreeHTML();
     } else {
-      window.setTimeout(checkIframeLoaded, 1000);
+      window.setTimeout(buildContentTree, 1000);
     }
   }
 }
 
-function buildContentTree() {
+function createContentTreeHTML() {
   const $pageNavigation = document.querySelector('.t3js-scaffold-content-navigation');
   const contentTree = document.createElement("div");
   contentTree.classList.add('content-tree');
-  contentTree.innerHTML = '<div class="headline">Content Tree</div>';
+  contentTree.innerHTML = '<label class="content-tree__headline">Content Tree</label>';
 
   // Create an empty string to store the HTML list
   const htmlList = document.createElement("ul");
 
   const contentArea = document.getElementById("typo3-contentIframe").contentWindow.document;
   const gridElements = contentArea.querySelectorAll(".t3-grid-cell");
-  console.log(gridElements);
   for (let i = 0; i < gridElements.length; i++) {
     const listEntry = document.createElement("li");
-    console.log(gridElements[i]);
-    listEntry.innerHTML = gridElements[i].find('.t3-page-column-header').textContent;
+    listEntry.innerHTML = gridElements[i].querySelector('.t3-page-column-header').textContent;
     htmlList.append(listEntry);
   }
 
@@ -49,13 +47,12 @@ window.addEventListener('load', (event) => {
   }*/
 
   if (window.top !== window) {
-    // Code is exectuted in an iframe
+    // Code is executed in an iframe
     //console.log('Die Datei wird in einem iFrame ausgeführt.');
   } else {
     // Code is only executed in main HTML
-    checkIframeLoaded();
+    buildContentTree();
   }
-
 });
 
 
