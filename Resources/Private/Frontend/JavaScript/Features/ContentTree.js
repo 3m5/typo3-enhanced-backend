717a397e0a6673078317969526887cf6ebe8b388
schneider.n <nicole.schneider@3m5.de>
2023-03-08 16:55:34 +0100

    T3ENCBE-104 use container name if available

--- Resources/Private/Frontend/JavaScript/Features/ContentTree.js
+++ Resources/Private/Frontend/JavaScript/Features/ContentTree.js
@@ -106,7 +106,7 @@ function createNestedList(rootElement, classList, createdTreeLinks) {
     let linkToContentElement = '';
     if(isGridContainer) {
       linkToContentElement = element.querySelector('.t3-page-ce-header a[title="Edit"]').cloneNode(true);
-      linkToContentElement.innerHTML = 'Container'
+      linkToContentElement.innerHTML = element.querySelector('.t3-page-ce-body .exampleContent').firstElementChild.tagName === 'STRONG' ? element.querySelector('.t3-page-ce-body').querySelector('.exampleContent > strong').textContent : 'Container';
       linkToContentElement.classList = '';
     } else {
       linkToContentElement = element.querySelector('.t3-page-ce-dragitem .exampleContent strong') ? stringToHTML(element.querySelector('.t3-page-ce-dragitem .exampleContent strong').innerHTML) : '';
