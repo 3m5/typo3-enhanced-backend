63539c337cfad6ed143bc650f89ada94b2ee3cbe
3m5\schneider.n <nicole.schneider@3m5.de>
2023-05-14 08:49:25 +0200

    add documentation, reload page only if user settings changes require that

--- Resources/Private/Frontend/JavaScript/Features/SaveSettingsListener.ts
+++ Resources/Private/Frontend/JavaScript/Features/SaveSettingsListener.ts
@@ -1,22 +1,76 @@
 import {featuresThatRequirePageReload} from "../Constants";
 
 function reloadPage() {
-  sessionStorage.removeItem('reloadPage');
   window.parent.location.reload();
 }
 
+/**
+ * the dialog informs the user, that a page reload is necessary so that he knows what is going on here
+ */
 function showPageReloadDialog() {
-  //const dialog = top.TYPO3.Modal.confirm("For all changes to take effect, the page must be reloaded once. Should the page be reloaded now?");
   const dialog = confirm("For all changes to take effect, the page must be reloaded once. Should the page be reloaded now?");
   if (dialog) {
     reloadPage();
   }
 }
 
+function initSaveSettings() {
+  const $saveButton = document.querySelector(".btn[name='data[save]']") as HTMLElement;
+  if(!!$saveButton) {
+    $saveButton.onclick = checkIfReloadIsNecessary;
+  }
+}
+
+/**
+ * compare origin feature values with new user settings and returns whether there is a change
+ * @param a
+ * @param b
+ */
+function compareFeatureArrays(a: { name: string, value: boolean }[], b: { name: string, value: boolean }[]): boolean {
+  // sort arrays by name
+  const sortedA = a.sort((x, y) => x.name.localeCompare(y.name));
+  const sortedB = b.sort((x, y) => x.name.localeCompare(y.name));
+
+  // compare each value for each name
+  for (let i = 0; i < sortedA.length; i++) {
+    if (sortedA[i].name !== sortedB[i].name || sortedA[i].value !== sortedB[i].value) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * checks if a feature, that requires page reload, is changed
+ * if yes, the page reload dialog is shown
+ */
+function checkIfReloadIsNecessary() {
+  const newFeatureValues: {name: string, value: boolean}[] = [];
+  featuresThatRequirePageReload.forEach(featureClassName => {
+    newFeatureValues.push({name: featureClassName, value: !!document.querySelector<HTMLInputElement>('[name="data[' + featureClassName + ']"]')?.checked});
+  });
+
+  const originFeatureValues = sessionStorage.getItem('originFeatureValues');
+  const parsedOriginFeatureValues = originFeatureValues ? JSON.parse(originFeatureValues) : [];
+  if(!compareFeatureArrays(parsedOriginFeatureValues, newFeatureValues)) {
+    setReloadTrigger();
+  }
+}
+
+/**
+ * to store all user settings correctly, we need to wait for TYPO3 saving everything and reload the settings iframe
+ * then we can reload the page, if the trigger is set
+ * if we would reload the page directly without this trigger, the user settings will not be stored correctly
+ */
 function setReloadTrigger() {
   sessionStorage.setItem('reloadPage', 'true');
 }
 
+/**
+ * features are grouped in backend area groups for a better structure of the user settings
+ * the user can toggle this backend area groups for a better focus
+ */
 function initSettingsGroupToggle() {
   document.querySelectorAll<HTMLElement>('.enba-uc-group__header').forEach(groupHeader => {
     groupHeader.addEventListener('click', function() {
@@ -25,12 +79,19 @@ function initSettingsGroupToggle() {
   });
 }
 
+/**
+ * disables all features and shows the Original TYPO3 backend
+ */
 function uncheckAllFeatures() {
   document.querySelectorAll<HTMLInputElement>('.enba-uc__feature input[type="checkbox"]').forEach(function(featureCheckbox) {
     featureCheckbox.checked = false
   });
 }
 
+/**
+ * if user chooses a preset, all features are toggled as defined in the Features.yaml
+ * @param selectedPreset
+ */
 function updateFeatures(selectedPreset : string | null) {
   switch(selectedPreset) {
     case 'custom':
@@ -58,6 +119,10 @@ function setPreset(presetValue : string) {
   }
 }
 
+/**
+ * if user chooses a preset, all features are toggled as defined in the Features.yaml
+ * if user changes a feature, the preset is automatically set to 'Custom'
+ */
 function initPresets() {
   document.querySelectorAll<HTMLInputElement>('[name="data[enba-presets]"]').forEach(function(radioButton) {
     radioButton.addEventListener('click', function() {
@@ -88,18 +153,31 @@ function initFeatureListener() {
           document.querySelector('html')?.classList.remove(featureClassName);
           window?.parent?.document?.querySelector('html')?.classList.remove(featureClassName);
         }
-      } else if(!!featureClassName && featuresThatRequirePageReload.includes(featureClassName)) {
-        setReloadTrigger();
       }
     });
   });
 }
 
+/**
+ * store initial values of every feature that requires page reload in session storage
+ * if user saves his changes, initial values are compared to the new ones to check if page reload is necessary
+ */
+function storeInitialFeatureValues() {
+  const featureValues: {name: string, value: boolean}[] = [];
+  featuresThatRequirePageReload.forEach(featureClassName => {
+    featureValues.push({name: featureClassName, value: !!document.querySelector<HTMLInputElement>('[name="data[' + featureClassName + ']"]')?.checked});
+  });
+  sessionStorage.setItem('originFeatureValues', JSON.stringify(featureValues))
+}
+
 export default function InitUserSettings() {
+  if(!!sessionStorage.getItem('reloadPage')) {
+    sessionStorage.removeItem('reloadPage')
+    showPageReloadDialog();
+  }
   initPresets();
   initSettingsGroupToggle();
   initFeatureListener();
-  if(!!sessionStorage.getItem('reloadPage')) {
-    showPageReloadDialog();
-  }
+  storeInitialFeatureValues();
+  initSaveSettings();
 }
